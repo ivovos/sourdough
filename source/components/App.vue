80ccd079c3fda4c5b2f0dddac6a78ad1a1c82448
ivo <vos.ivo@gmail.com>
2017-09-29 16:45:31 +0100

    Added moment.js

--- source/components/App.vue
+++ source/components/App.vue
@@ -1,32 +1,44 @@
 <template>
 	<div id='app'>
 		<h1>What time would you like fresh bread?</h1>
-		<div class="day-picker"
-		<div class="time-picker">
-			<swiper :options="sliderOptions" class='pad' ref="hour">
-			
 
-				<swiper-slide v-for="slide in swiperSlidesHours" :key="slide.id">
-					<span class="numbers-large">{{ slide.toString()}}</span>
-				</swiper-slide>
+			<div class="day-time-picker">
+				<div class="day-picker">
+					<div class="small-caps">saturday</div>
+				</div>
+				<div class="time-picker">
+					<swiper :options="sliderOptions" class='pad' ref="hour">
+					
 
-			</swiper>
-			<div class="colon"><span class="numbers-large">:</span></div>
-			<swiper :options="sliderOptions" class='pad' ref="minutes">
+						<swiper-slide v-for="slide in swiperSlidesHours" :key="slide.id">
+							<span class="numbers-large">{{ slide.toString()}}</span>
+						</swiper-slide>
 
-				<swiper-slide v-for="slide in swiperSlidesMinutes" :key="slide.id">
-					<span class="numbers-large">{{ slide.toString()}}</span>
-				</swiper-slide>
+					</swiper>
+					<div class="colon"><span class="numbers-large">:</span></div>
+					<swiper :options="sliderOptions" class='pad' ref="minutes">
 
-			</swiper>
+						<swiper-slide v-for="slide in swiperSlidesMinutes" :key="slide.id">
+							<span class="numbers-large">{{ slide.toString()}}</span>
+						</swiper-slide>
+
+					</swiper>
+				</div>
+			</div>
+
+		<div class="result">
+			<div class="label small-caps">Start at</div>
+			<div class="medium-text">{{ startDay }} , {{ startTime }}</div>
 		</div>
-		<button v-on:click="getValue">Create plan</button>
+		<button v-on:click="getValue">Start Plan</button>
 	</div>
 </template>
 
 <script>
 
 import data from 'js/data'
+import Moment from 'moment'
+
 
 export default {
 	name: 'app',
@@ -41,7 +53,9 @@ export default {
 			},
 			swiperSlidesHours: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24],
 			swiperSlidesMinutes: ['00','05',10,15,20,25,30,35,40,45,50,55],
-			steps: data.steps
+			steps: data.steps,
+			startDay: "Friday",
+			startTime: "10:40"
 		}
 	},
 	methods: {
@@ -54,7 +68,7 @@ export default {
    
     },
 	mounted() {
-		// console.log(data.steps)
+		console.log(Moment.now());
 	}
 }
 </script>
@@ -68,21 +82,38 @@ export default {
 	flex-direction: column;
 	justify-content: space-between;
 	.pad{
-/*		background:#EEE;
-*/		width:7em;
+		width:7em;
 		height:120px;
 		margin: 5%;
+	}
+	h1{
+		font-size: 2em;
+	}
+	.day-time-picker{
+		display: flex;
+		flex-direction: column;
+		text-align: center;
 	}
 	.time-picker{
 		display: flex;
 		flex-direction: row;
-		text-align: center;
+	}
+	.small-caps{
+		text-transform: uppercase;
+		font-weight: 500;
+		letter-spacing: 1px;
 	}
 	.colon{
 		width: 20px;
 	}
 	.numbers-large{
-		font-size: 96px;
+		font-size: 6em;
+		text-align: center;
+	}
+	.medium-text{
+		font-size: 1.125em;
+	}
+	.result{
 		text-align: center;
 	}
 	button{
